80baa398aebdbd959cf70c4d496a038c4a7ee028
choijihye <choijihye295@jbnu.ac.kr>
2026-02-16 15:04:40 +0900

    write 페이지 및 add 생성

--- src/main/java/com/jbnu/cjh/shop/Item.java
+++ src/main/java/com/jbnu/cjh/shop/Item.java
@@ -1,19 +1,23 @@
 package com.jbnu.cjh.shop;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @ToString
+@Getter
+@Setter
 public class Item { //Entity -> Item이름으로 테이블 생성해줌
     //테이블에 어떤 컬럼 들어갈지 정의해야함
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //자동으로 1씩 증가하다록
-    public Long id; //id컬럼에는 @Id붙여주자
+    private Long id; //id컬럼에는 @Id붙여주자
 
     //@Column(nullable = false) //이 컬럼에 제한을 주기
 
-    public String title;
-    public Integer price;
+    private String title;
+    private Integer price;
 
 
 }
--- src/main/java/com/jbnu/cjh/shop/ItemController.java
+++ src/main/java/com/jbnu/cjh/shop/ItemController.java
@@ -5,10 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller //상품관련 API 여기다가 모으자
 @RequiredArgsConstructor
@@ -16,24 +16,29 @@ public class ItemController {
     //리포지토리 인터페이스 등록
     private final ItemRepository itemRepository;
 
-    //Loombok 없이 리포지토리 등록하려면
-    /*@Autowired
-    public ItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }*/
-
-
     @GetMapping("/list") //이 URL접속
     String list(Model model){
         //테이블에서 데이터 꺼내기 (JPA로 데이터 입출력)
         //리포지토리에서 DB입출력 함수 쓰기
         List<Item> result = itemRepository.findAll(); //list자료형으로 가져옴
-        //System.out.println(result.get(0).price);
-        var a = new Item();
-        System.out.println(a.toString());
 
         model.addAttribute("items", result);
         return "list.html"; //이거 보내주세요
         //템플릿 엔진 > html을 그때그때 만들어사 사용가능
+    }
+
+    @GetMapping("/write")
+    String write(){
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    public String addPost(String title, Integer price) {
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+
+        itemRepository.save(item);
+        return "redirect:/list";
     }
 }
